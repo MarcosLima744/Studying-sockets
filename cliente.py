5b30e1a22d0a8373a0ed80c1d5075b8244ed2a80
MarcosLima744 <71466551+MarcosLima744@users.noreply.github.com>
2022-06-19 15:20:41 -0300

    Update cliente.py

--- cliente.py
+++ cliente.py
@@ -1,23 +1,41 @@
-import socket
-import threading 
+import socket 
 import traceback
-
-
+import _thread
 
 serverName = 'localhost'
 serverPort = 12000
 
-
 try:
     clientSocket = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
     clientSocket.connect((serverName, serverPort))
-    data = input()
-    data = data.encode('ascii')
-    clientSocket.send(data)
-    setence = clientSocket.recv(1024)
-    setence.decode()
-    print(setence)
-    clientSocket.close()
-
 except Exception:
     print(traceback.format_exc())
+
+
+def sendMessage(Socket):
+    while True:
+        data = input()
+        data = data.encode('utf-8')
+        try:
+            Socket.send(data)
+        except Exception:
+            print(traceback.format_exc())
+
+
+def recMessage(Socket):
+    while True:
+        try:
+            mensagem = Socket.recv(3096)
+            mensagem = mensagem.decode('utf-8')
+            print(mensagem)
+
+        except Exception:
+            print(traceback.format_exc())
+
+
+
+_thread.start_new_thread(sendMessage, (clientSocket,))
+_thread.start_new_thread(recMessage, (clientSocket,))
+
+while True:
+    pass
